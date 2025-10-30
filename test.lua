fffc3d7bd3e7ca64ab31eeb8e4e3dd84e475ef1e
冷 <1545246506@qq.com>
2025-10-31 07:12:01 +0800

    Add files via upload

--- test.lua
+++ test.lua
@@ -1,5 +1,6 @@
 return {
     ["cc6721d2-0c32-4ccd-a5cd-2c5f83160835"] = "Test",
     ["32ab1d21-4ecc-4849-87da-dc5f6365f949"] = "tidiudussursrududusgjfjd",
+    ["77933661-a0f0-4e15-a85d-ed827a81dba9"] = "ajdishkhlnsjshoash",
     [""] = ""
 }
